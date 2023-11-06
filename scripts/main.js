ded58f0062c9d94a18537971e6ba0f5f6758634a
Inez Yoon <inez3cs@gmail.com>
2023-11-05 17:33:15 -0800

    Add firebase for userinfo

--- scripts/main.js
+++ scripts/main.js
@@ -76,3 +76,72 @@ function readUserEmail(useremail) {
     )
 }
 readUserEmail("user-info-details");
+
+function readUserContactMethod(usercontactmethod) {
+    db.collection("user-info").doc(usercontactmethod) 
+    .onSnapshot(whatgoeshere => {
+        console.log(whatgoeshere.data());
+        document.getElementById("user-contactmethod").innerHTML = whatgoeshere.data().userContactMethod;
+
+    }
+    )
+}
+readUserContactMethod("user-info-details");
+
+//appointment history
+function readUserClinicName(userclinicname) {
+    db.collection("user-info").doc(userclinicname) 
+    .onSnapshot(whatgoeshere => {
+        console.log(whatgoeshere.data());
+        document.getElementById("user-clinicname").innerHTML = whatgoeshere.data().userAppointmentHistory["clinicName"];
+
+    }
+    )
+}
+readUserClinicName("user-info-details");
+
+function readUserClinicLocation(usercliniclocation) {
+    db.collection("user-info").doc(usercliniclocation) 
+    .onSnapshot(whatgoeshere => {
+        console.log(whatgoeshere.data());
+        document.getElementById("user-cliniclocation").innerHTML = whatgoeshere.data().userAppointmentHistory["clinicLocation"];
+
+    }
+    )
+}
+readUserClinicLocation("user-info-details");
+
+function readUserVisitingDate(uservisitingdate) {
+    db.collection("user-info").doc(uservisitingdate) 
+    .onSnapshot(whatgoeshere => {
+        console.log(whatgoeshere.data());
+        document.getElementById("user-visitingdate").innerHTML = whatgoeshere.data().userAppointmentHistory["clinicVisitingDate"];
+
+    }
+    )
+}
+readUserVisitingDate("user-info-details");
+
+
+function readUserMyRatings(usermyratings) {
+    db.collection("user-info").doc(usermyratings) 
+    .onSnapshot(whatgoeshere => {
+        console.log(whatgoeshere.data());
+        document.getElementById("user-myratings").innerHTML = whatgoeshere.data().userAppointmentHistory["clinicMyRatings"];
+
+    }
+    )
+}
+readUserMyRatings("user-info-details");
+
+
+function readUserFavourite(userfavourite) {
+    db.collection("user-info").doc(userfavourite) 
+    .onSnapshot(whatgoeshere => {
+        console.log(whatgoeshere.data());
+        document.getElementById("user-favourite").innerHTML = whatgoeshere.data().userAppointmentHistory["clinicFavourite"];
+
+    }
+    )
+}
+readUserFavourite("user-info-details");
