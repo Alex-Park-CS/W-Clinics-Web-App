9553eb5b227f648e05d12d4e9ce151a8b3614fe9
Alex-Park-CS <alexpark0930@gmail.com>
2023-11-13 13:58:24 -0800

    finished clinic profile dynamically

--- scripts/clinic_profile_page.js
+++ scripts/clinic_profile_page.js
@@ -11,14 +11,24 @@ function displayClinicInfo() {
         .then(doc => {
             thisClinic = doc.data();
             clinicCode = thisClinic.code;
-            clinicName = doc.data().name;
+            clinicName = doc.data().clinicName;
             clinicAddress = doc.data().address;
+            clinicHours = doc.data().hours
+            clinicRating = doc.data().rating
+            clinicWaitTime = doc.data().wait_time_minutes
+            clinicWalkin = doc.data().walkin_availibility
+            clinicCode = doc.data().code
+
 
 
             // only populate title, and image
             document.getElementById("clinicName").innerHTML = clinicName;
-            document.getElementById("clinicAddress").innerHTML = clinicAddress;
-            let imgEvent = document.querySelector(".hike-img");
+            document.getElementById("clinicAddress").innerHTML = "Address: " + clinicAddress;
+            document.getElementById("clinic-hours").innerHTML = clinicHours
+            document.getElementById("clinic-waittime").innerHTML = "Wait Time: " + clinicWaitTime + "min"
+            document.getElementById("clinic-walkin").innerHTML = "Walkin Availibility: " + clinicWalkin
+            document.getElementById("clinic-rating").innerHTML = "Rating: " + clinicRating
+            let imgEvent = document.querySelector(".clinic-img");
             imgEvent.src = "../images/" + clinicCode + ".jpg";
         });
 }
