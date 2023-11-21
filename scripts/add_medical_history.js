07d77cab49212c12b7df7c96a9deebdf09ca6780
Inez Yoon <inez3cs@gmail.com>
2023-11-21 10:16:08 -0800

    Update medical history

--- scripts/add_medical_history.js
+++ scripts/add_medical_history.js
@@ -3,11 +3,13 @@ function submitMedicalHistory() {
     console.log("medical history form");
     let historyRespi = document.getElementById("history-respi").value;
     let historyCardio = document.getElementById("history-cardio").value;
+    let historyVision = document.getElementById("history-vision").value;
+    let historyEar = document.getElementById("history-ear").value;
     let historyInfection = document.getElementById("history-infection").value;
-    
+    let historyMental = document.getElementById("history-mental").value;
+    let historyJoint = document.getElementById("history-joint").value;
 
-
-    console.log(historyRespi, historyCardio, historyInfection);
+    console.log(historyRespi, historyCardio, historyVision, historyInfection, historyMental, historyJoint);
 
     var user = firebase.auth().currentUser;
     if (user) {
@@ -19,7 +21,13 @@ function submitMedicalHistory() {
         db.collection("user-info").add({
             respitoryHistoryFB: historyRespi,
             cardioHistoryFB: historyCardio,
+            visionHistoryFB: historyVision,
+            earHistoryFB: historyEar,
+            infectHistoryFB: historyInfection,
+            mentalHistoryFB: historyMental,
             infectionHIstoryFB: historyInfection,
+            
+
         }).then(() => {
             window.location.href = "thanks_medical_history.html"; // Redirect to the thanks page
         });
