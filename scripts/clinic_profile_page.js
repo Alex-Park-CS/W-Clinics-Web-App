82466b7c4df23212f04f0d83c6698a52f4f60fbd
Richard Li    <rli108@my.bcit.ca>
2023-11-20 22:33:35 -0800

    Add favourites page

--- scripts/clinic_profile_page.js
+++ scripts/clinic_profile_page.js
@@ -13,19 +13,19 @@ function displayClinicInfo() {
             clinicCode = thisClinic.code;
             clinicName = doc.data().clinicName;
             clinicAddress = doc.data().address;
-            clinicHours = doc.data().hours
-            clinicRating = doc.data().rating
-            clinicWaitTime = doc.data().wait_time_minutes
-            clinicWalkin = doc.data().walkin_availibility
-            clinicCode = doc.data().code
+            clinicHours = doc.data().hours;
+            clinicRating = doc.data().rating;
+            clinicWaitTime = doc.data().wait_time_minutes;
+            clinicWalkin = doc.data().walkin_availibility;
+            clinicCode = doc.data().code;
 
 
             document.getElementById("clinicName").innerHTML = clinicName;
             document.getElementById("clinicAddress").innerHTML = "Address: " + clinicAddress;
-            document.getElementById("clinic-hours").innerHTML = clinicHours
+            document.getElementById("clinic-hours").innerHTML = clinicHours;
             document.getElementById("clinic-waittime").innerHTML = "Wait Time: " + clinicWaitTime + "min"
-            document.getElementById("clinic-walkin").innerHTML = "Walkin Availibility: " + clinicWalkin
-            document.getElementById("clinic-rating").innerHTML = "Rating: " + clinicRating
+            document.getElementById("clinic-walkin").innerHTML = "Walkin Availibility: " + clinicWalkin;
+            document.getElementById("clinic-rating").innerHTML = "Rating: " + clinicRating;
             let imgEvent = document.querySelector(".clinic-img");
             imgEvent.src = "../images/" + clinicCode + ".jpg";
             document.querySelector('i').id = 'save-'+ ID; // for assigning unique id to each element
