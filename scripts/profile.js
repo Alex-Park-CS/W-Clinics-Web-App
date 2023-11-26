a7ad35d013a26512edccc976ece00ca710ef8708
Inez Yoon <inez3cs@gmail.com>
2023-11-26 15:32:15 -0800

    Update profile section to populate and save info

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,110 +1,190 @@
-var currentUser;               //points to the document of the user who is logged in
+var currentUser;
+
+// Function to populate user information
 function populateUserInfo() {
-            firebase.auth().onAuthStateChanged(user => {
-                // Check if user is signed in:
-                if (user) {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            // Go to the correct user document by referencing the user uid
+            currentUser = db.collection("users").doc(user.uid);
 
-                    //go to the correct user document by referencing to the user uid
-                    currentUser = db.collection("users").doc(user.uid)
-                    //get the document for current user.
-                    currentUser.get()
-                        .then(userInfo => {  
-                            //get the data fields of the user
-                            var userFirstName = userInfo.data().FirstName;
-                            var userMiddleName = userInfo.data().MiddleName;
-                            var userLastName = userInfo.data().LastName;
-                            var userDOB = userInfo.data().DOB;
-                            var userGender = userInfo.data().Gender;
-                            var userEmail = userInfo.data().email;
-                            var userPhone = userInfo.data().Phone;
+            // Get the document for the current user
+            currentUser.get()
+                .then(userInfo => {
+                    if (userInfo.exists) {
+                        // Check if the user document exists
+                        var data = userInfo.data();
 
-                            //if the data fields are not empty, then write them in to the form.
-                            if (userFirstName != null) {
-                                document.getElementById("FirstNameInput").value = userFirstName;
-                            }
-                            if (userMiddleName != null) {
-                                document.getElementById("MiddleNameInput").value = userMiddleName;
-                            }
-                            if (userLastName != null) {
-                                document.getElementById("LastNameInput").value = userLastName;
-                            }
-                            if (userDOB != null) {
-                                document.getElementById("DateOfBirthInput").value = userDOB;
-                            }
-                            if (userGender != null) {
-                                document.getElementById("genderInput").value = userGender;
-                            }
-                            if (userEmail != null) {
-                                document.getElementById("emailInput").value = userEmail;
-                            }
-                            if (userPhone != null) {
-                                document.getElementById("phoneInput").value = userPhone;
-                            }
-                        })
-                } else {
-                    // No user is signed in.
-                    console.log ("No user is signed in");
-                }
-            });
+                        // Get the data fields of the user
+                        var userFirstName = data.userFirstName;
+                        var userMiddleName = data.userMiddleName;
+                        var userLastName = data.userLastName;
+                        var userDOB = data.userDOB;
+                        var userGender = data.userGender;
+                        var userEmail = data.userEmail;
+                        var userPhone = data.userPhone;
+
+                        // Update form fields with user data
+                        if (userFirstName != null) {
+                            document.getElementById("FirstNameInput").value = userFirstName;
+                        }
+                        if (userMiddleName != null) {
+                            document.getElementById("MiddleNameInput").value = userMiddleName;
+                        }
+                        if (userLastName != null) {
+                            document.getElementById("LastNameInput").value = userLastName;
+                        }
+                        if (userDOB != null) {
+                            document.getElementById("DateOfBirthInput").value = userDOB;
+                        }
+                        if (userGender != null) {
+                            document.getElementById("genderInput").value = userGender;
+                        }
+                        if (userEmail != null) {
+                            document.getElementById("emailInput").value = userEmail;
+                        }
+                        if (userPhone != null) {
+                            document.getElementById("phoneInput").value = userPhone;
+                        }
+
+                        
+                    } else {
+                        console.log("New user!");
+                        currentUser = db.collection("users").doc(user.uid);
+
+                        // Additional logic for new user...
+                        currentUser.get()
+                            .then(userInfo => {
+                                if (userInfo.exists) {
+                                    // Check if the user document exists
+                                    var data = userInfo.data();
+                                    db.collection("users").add({
+
+                                    // Get the data fields of the user
+                                    // var userFirstName = data.userFirstName;
+                                    // var userMiddleName = data.userMiddleName;
+                                    // var userLastName = data.userLastName;
+                                    // var userDOB = data.userDOB;
+                                    // var userGender = data.userGender;
+                                    // var userEmail = data.userEmail;
+                                    // var userPhone = data.userPhone;
+                                    userFirstName : FirstNameInput,
+                                    userMiddleName : MiddleNameInput,
+                                    userLastName : LastNameInput,
+                                    userDOB : DateOfBirthInput,
+                                    userGender : genderInput,
+                                    userEmail :emailInput,
+                                    userPhone : phoneInput,
+
+
+                                    })
+                                }
+                            })
+                            .catch(error => {
+                                console.error("Error getting user document:", error);
+                            });
+                    }
+                });
+        } else {
+            console.log("No user is signed in");
         }
-
-//call the function to run it 
-populateUserInfo();        
-
-function editUserInfo() {
-    document.getElementById('personalInfoFields').disabled = false;
- }
-
-function saveUserInfo() {
-    //get entered information by user
-    userFirstName = document.getElementById("FirstNameInput").value;
-    userMiddleName = document.getElementById("MiddleNameInput").value;
-    userLastName = document.getElementById("LastNameInput").value;
-    userDOB = document.getElementById("DateOfBirthInput").value;
-    userGender = document.getElementById("genderInput").value;
-    userEmail = document.getElementById("emailInput").value;
-    userPhone = document.getElementById("phoneInput").value;
-
-    console.log("medical history form");
-    let historyRespi = document.getElementById("history-respi").value;
-    let historyCardio = document.getElementById("history-cardio").value;
-    let historyVision = document.getElementById("history-vision").value;
-    let historyEar = document.getElementById("history-ear").value;
-    let historyInfection = document.getElementById("history-infection").value;
-    let historyMental = document.getElementById("history-mental").value;
-    let historyJoint = document.getElementById("history-joint").value;
-
-
-    console.log(historyRespi, historyCardio, historyVision, historyInfection, historyMental, historyJoint);
-
-
-
-    // update the user document in firestore
-    currentUser.update({
-        FirstName: userFirstName,
-        MiddleName: userMiddleName, 
-        LastName: userLastName,
-        DOB: userDOB,
-        Gender: userGender,
-        email: userEmail,
-        Phone: userPhone,
-        ContactMethod: userContactMethod,
-        respitoryHistoryFB: historyRespi,
-        cardioHistoryFB: historyCardio,
-        visionHistoryFB: historyVision,
-        earHistoryFB: historyEar,
-        infectHistoryFB: historyInfection,
-        mentalHistoryFB: historyMental,
-        infectionHIstoryFB: historyInfection,
-    })
-    .then(() => {
-        console.log("Document successfully updated!");
-    })
-
+    });
 }
 
-//add medical history page
-function saveUserInfo(){
-    console.log("save user information")
+// Call the function to run it
+populateUserInfo();
+
+// Function to enable editing user information
+function editUserInfo() {
+    document.getElementById('personalInfoFields').disabled = false;
+}
+
+// Function to save user information
+// Function to save user information
+function saveUserInfo() {
+    // Get entered information by the user
+    var userFirstName = document.getElementById("FirstNameInput").value;
+    var userMiddleName = document.getElementById("MiddleNameInput").value;
+    var userLastName = document.getElementById("LastNameInput").value;
+    var userDOB = document.getElementById("DateOfBirthInput").value;
+    var userGender = document.getElementById("genderInput").value;
+    var userEmail = document.getElementById("emailInput").value;
+    var userPhone = document.getElementById("phoneInput").value;
+
+    // Get medical history information
+    var historyRespi = document.getElementById("history-respi").value;
+    var historyCardio = document.getElementById("history-cardio").value;
+    var historyVision = document.getElementById("history-vision").value;
+    var historyEar = document.getElementById("history-ear").value;
+    var historyInfection = document.getElementById("history-infection").value;
+    var historyMental = document.getElementById("history-mental").value;
+    var historyJoint = document.getElementById("history-joint").value;
+
+    console.log("Medical history form:", historyRespi, historyCardio, historyVision, historyInfection, historyMental, historyJoint);
+
+    // Check if the user document exists
+    currentUser.get()
+        .then(doc => {
+            if (doc.exists) {
+                // Update the existing user document in Firestore
+                currentUser.update({
+                    userFirstName: userFirstName,
+                    userMiddleName: userMiddleName,
+                    userLastName: userLastName,
+                    userDOB: userDOB,
+                    userGender: userGender,
+                    userEmail: userEmail,
+                    userPhone: userPhone,
+                    respitoryHistoryFB: historyRespi,
+                    cardioHistoryFB: historyCardio,
+                    visionHistoryFB: historyVision,
+                    earHistoryFB: historyEar,
+                    infectHistoryFB: historyInfection,
+                    mentalHistoryFB: historyMental,
+                    infectionHistoryFB: historyInfection,
+                })
+                .then(() => {
+                    console.log("Document successfully updated!");
+                    window.location.href = "thanks_medical_history.html";
+                })
+                .catch(error => {
+                    console.error("Error updating user document:", error);
+                });
+            } else {
+        
+
+                // For a new user, create a new document in Firestore
+                currentUser.set({
+                    userFirstName: userFirstName,
+                    userMiddleName: userMiddleName,
+                    userLastName: userLastName,
+                    userDOB: userDOB,
+                    userGender: userGender,
+                    userEmail: userEmail,
+                    userPhone: userPhone,
+                    respitoryHistoryFB: historyRespi,
+                    cardioHistoryFB: historyCardio,
+                    visionHistoryFB: historyVision,
+                    earHistoryFB: historyEar,
+                    infectHistoryFB: historyInfection,
+                    mentalHistoryFB: historyMental,
+                    infectionHistoryFB: historyInfection,
+                })
+                .then(() => {
+                    console.log("New user document created!");
+                    window.location.href = "thanks_medical_history.html";
+                })
+                .catch(error => {
+                    console.error("Error creating new user document:", error);
+                });
+            }
+        })
+        .catch(error => {
+            console.error("Error getting user document:", error);
+        });
+}
+
+// Function to save medical history and navigate to the next page
+function saveMedicalHistory() {
+    console.log("Save medical history");
     window.location.href = "thanks_medical_history.html";
 }
