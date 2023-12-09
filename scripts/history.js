3a9f1d6c8973e6e706b54e3e9f2f2b2b69f3b85e
Inez Yoon <inez3cs@gmail.com>
2023-12-08 18:50:46 -0800

    Fix error for history

--- scripts/history.js
+++ scripts/history.js
@@ -1,64 +1,63 @@
-// Function to retrieve and display user's appointments
 function getAppointmentsInfo(currentUser) {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
             var userID = user.uid;
 
-            // Get user document from Firestore
             db.collection("users").doc(userID).get().then(userDoc => {
-                // Retrieve user's appointments from the document
-                var appointments = userDoc.data().appointments;
+                var userAppointmentsData = userDoc.data();
 
-                console.log("Appointments:", appointments);
+                if (userAppointmentsData && userAppointmentsData.appointments && userAppointmentsData.appointments.length > 0) {
+                    var appointments = userAppointmentsData.appointments;
 
-                // Select the container where you want to append appointments
-                var appointmentsContainer = document.getElementById("appointments-container");
+                    console.log("Appointments:", appointments);
 
-                if (!appointmentsContainer) {
-                    console.error("Appointments container not found");
-                    return;
-                }
+                    // Select the container where you want to append appointments
+                    var appointmentsContainer = document.getElementById("appointments-container");
 
-                // Iterate through each appointment and display its information
-                appointments.forEach(appointment => {
-                    var clinicID = appointment.clinicID;
+                    if (!appointmentsContainer) {
+                        console.error("Appointments container not found");
+                        return;
+                    }
 
-                    // Get clinic document from Firestore
-                    db.collection("clinics").doc(clinicID).get().then(clinicDoc => {
-                        // For debugging
-                        console.log("Clinic Data:", clinicDoc.data()); 
+                    appointments.forEach(appointment => {
+                        var clinicID = appointment.clinicID;
 
-                        // Actual code
-                        var clinicName = clinicDoc.data().clinicName;
-                        var clinicAddress = clinicDoc.data().address;
-                        var userAppmntDate = appointment.userAppmntDate;
-                        var userAppmntTime = appointment.userAppmntTime;
-                        var docID = clinicDoc.id;
+                        db.collection("clinics").doc(clinicID).get().then(clinicDoc => {
+                            // For debugging
+                            console.log("Clinic Data:", clinicDoc.data()); 
 
-                        // Create HTML elements for each appointment
-                        var appointmentDiv = document.createElement("div");
-                        appointmentDiv.className = "card text-center";
-                        appointmentDiv.style.width = "18rem";
-                        appointmentDiv.innerHTML = `
-                            <div class="card-body">
-                                <div class="card-title"><h4>${clinicName}</h4></div>
-                                <div class="card-text">${clinicAddress}</div>
-                                <p>Appointment Date: <br>${userAppmntDate}</p>
-                                <p>Appointment Time: <br>${userAppmntTime}</p>
-                                <a href="clinic_profile_page.html?docID=${docID}" class="btn btn-success">To clinic page</a>
-                                <br><br>
-                            </div>
-                        `;
+                            // Actual code
+                            var clinicName = clinicDoc.data().clinicName;
+                            var clinicAddress = clinicDoc.data().address;
+                            var userAppmntDate = appointment.userAppmntDate;
+                            var userAppmntTime = appointment.userAppmntTime;
+                            var docID = clinicDoc.id;
 
-                        // Append the appointment information to the container
-                        appointmentsContainer.appendChild(appointmentDiv);
+                            // Create HTML elements for each appointment
+                            var appointmentDiv = document.createElement("div");
+                            appointmentDiv.className = "card text-center";
+                            appointmentDiv.style.width = "18rem";
+                            appointmentDiv.innerHTML = `
+                                <div class="card-body">
+                                    <div class="card-title"><h4>${clinicName}</h4></div>
+                                    <div class="card-text">${clinicAddress}</div>
+                                    <p>Appointment Date: <br>${userAppmntDate}</p>
+                                    <p>Appointment Time: <br>${userAppmntTime}</p>
+                                    <a href="clinic_profile_page.html?docID=${docID}" class="btn btn-success">To clinic page</a>
+                                    <br><br>
+                                </div>
+                            `;
 
-                        // Update bookmark status on click
-                        document.getElementById(`save-${docID}`).onclick = () => updateBookmark(currentUser, docID);
-                    }).catch(error => {
-                        console.error("Error getting clinic document: ", error);
+                            // Append the appointment information to the container
+                            appointmentsContainer.appendChild(appointmentDiv);
+
+                            // Update bookmark status on click
+                            document.getElementById(`save-${docID}`).onclick = () => updateBookmark(currentUser, docID);
+                        }).catch(error => {
+                            console.error("Error getting clinic document: ", error);
+                        });
                     });
-                });
+                } 
             }).catch(error => {
                 console.error("Error getting user document: ", error);
             });
@@ -72,9 +71,7 @@ function getAppointmentsInfo(currentUser) {
 window.onload = function () {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
-            // Set the global currentUser variable to the current user's Firestore document
             var currentUser = firebase.firestore().collection("users").doc(user.uid);
-            // Retrieve and display user's appointments
             getAppointmentsInfo(currentUser);
         } else {
             console.log("No user is signed in");
